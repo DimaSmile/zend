88378d4451452ab6b53d0389f78dab0d85b0ac0c
DimaSmile <efremov2293@gmail.com>
2017-06-23 19:46:54 +0300

    add add.phtml

--- module/Admin/src/Admin/Controller/CategoryController.php
+++ module/Admin/src/Admin/Controller/CategoryController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace Admin\Controller;
+
 use Application\Controller\BaseAdminController as BaseController;
+use Admin\Form\CategoryAddForm;
+use Blog\Entity\Category;
 
 
 class CategoryController extends BaseController
@@ -12,5 +15,41 @@ class CategoryController extends BaseController
         $rows =  $query->getResult();
         
         return array('category' => $rows);
+    }
+    
+    public function addAction() {
+        
+        $form = new CategoryAddForm;
+        $status = $message = '';
+        $em = $this->getEntityManager();
+        
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                
+                $category = new Category();
+                $category->exchangeArray($form->getData());
+                
+                $em->persist($category);
+                $em->flush();
+                
+                $status = 'success';
+                $message = 'Категория добавлена';
+            }else{
+                $status = 'error';
+                $message = 'Ошибка параметров';
+            }
+        } else {
+            return array('form' => $form);
+        }
+        
+        if ($message) {
+            $this->flashMessenger()
+                    ->setNamespace($status)
+                    ->addMessage($message);
+        }
+        
+        return $this->redirect()->toRoute('admin/category');
     }
 }
